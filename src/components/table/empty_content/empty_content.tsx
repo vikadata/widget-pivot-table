91f909501ac4aa38fc313150cbcd47abb3da9af4
Niko030303 <y1257891258@qq.com>
2022-03-22 22:49:15 +0800

    feat: 增加条件筛选行记录（临时方案）

--- src/components/table/empty_content/empty_content.tsx
+++ src/components/table/empty_content/empty_content.tsx
@@ -2,28 +2,31 @@ import React, { memo } from 'react';
 import { t } from '@vikadata/widget-sdk';
 import { black } from '@vikadata/components';
 import { Strings } from '../../../utils';
+import { BaseTableProps } from 'ali-react-table';
 
-export const DefaultEmptyContent = memo(() => {
-  return (
-    <>
-      <img 
-        alt="empty-image" 
-        src="//s1.vika.cn/space/2022/02/16/7f9dca59465c4ac8be91dbbf1ab4abad?attname=default_pivot.png" 
-        style={{
-          width: 160,
-          height: 120
-        }}
-      />
-      <div 
-        style={{
-          minWidth: 210,
-          marginTop: 8,
-          lineHeight: 1.5,
-          color: black[1000]
-        }}
-      >
-        {t(Strings.pivot_table_no_data)}
-      </div>
-    </>
-  );
-});
+export function defaultEmptyContent(baseTableProps: BaseTableProps) {
+  return memo(() => {
+    return (
+      <>
+        <img 
+          alt="empty-image" 
+          src="//s1.vika.cn/space/2022/02/16/7f9dca59465c4ac8be91dbbf1ab4abad?attname=default_pivot.png" 
+          style={{
+            width: 160,
+            height: 120
+          }}
+        />
+        <div 
+          style={{
+            minWidth: 210,
+            marginTop: 8,
+            lineHeight: 1.5,
+            color: black[1000]
+          }}
+        >
+          {(baseTableProps?.columns.length === 1 && baseTableProps?.dataSource.length === 0) ? t(Strings.pivot_table_no_data) : t(Strings.pivot_table_filter_result_is_empty)}
+        </div>
+      </>
+    );
+  });
+}
